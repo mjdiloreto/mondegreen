264fd071eb30f9809538ba5b1ca262c53aaaf7f1
Matthew DiLoreto <mjdiloreto@gmail.com>
2020-06-29 09:53:46 +0000

    Add filtering by word popularity, but the reference list does not include contractions, which are necessary. Will need a new way to filter CMU dict.

--- src/mondegreen/mondegreen.clj
+++ src/mondegreen/mondegreen.clj
@@ -3,6 +3,13 @@
             [clojure.set :refer [union]]
             [clojure.math.combinatorics :as combo]))
 
+;;; Configuration
+(def word-popularity-cutoff 100000)
+
+
+
+;;; Word popularity
+;;; ISSUE: This set does not seem to contain contractions like "I'll"
 (def word-counts
   (->> (slurp "resources/count_1w.txt")
        (string/split-lines)
@@ -14,7 +21,13 @@
   [n]
   (set (take n word-counts)))
 
-(def maps-parts
+(def words-to-include (most-common-words word-popularity-cutoff))
+
+
+
+;;; Dictionary file Parsing
+(defn make-maps-parts
+  [word-filter]
   (let [dictionary-chars #"^[A-Z'\-\.\_]+" ; All of these characters valid characters an English word can contain.
         symbol->phone (fn [sym] (re-find dictionary-chars sym))
         syms (->>
@@ -30,10 +43,15 @@
          (filter (complement empty?))
          (map #(map symbol->phone %)) ; TODO revise. at this point they are all nested operations.
          (filter (fn [[spelling _tab & pronunciation]]
-                    (every? (partial contains? syms) pronunciation)))
+                   (every? (partial contains? syms) pronunciation)))
+         (filter (fn [[spelling _tab & pronunciation]]
+                   (word-filter spelling)))
          (map (fn [[spelling _tab & pronunciation]]
                 [{spelling [pronunciation]} {pronunciation [spelling]}])) ; create both maps at once, spelling->pronunciation, pronunciation->spelling
          )))
+
+(def maps-parts (make-maps-parts identity)) ; uncomment if we want to use all words, not just the n most popular
+;(def maps-parts (make-maps-parts (fn [spelling] (contains? words-to-include spelling)))) ; uncomment if we want to use just the n most popular words
 
 (def word->pronunciations
   (reduce (partial merge-with into) (map first maps-parts)))
@@ -62,8 +80,17 @@
   (word->pronunciations "ICE-CREAM")   ;  [("AY" "S" "K" "R" "IY" "M")]
   (word->pronunciations "I")           ;  [("AY")]
   (word->pronunciations "SCREAM")      ;  [("S" "K" "R" "IY" "M")]
+
+  "An interesting note is we can use these data structures to answer questions about the frequencies of homophones"
+  (count maps-parts)                   ; 133810
+  (count word->pronunciations)         ; 125011
+  (count pronunciation->word)          ; 113720
+  "This means it is more common that a pronunciation maps to multiple words than a word having multiple pronunciations."
   )
 
+
+
+;;; Dictionary Searching
 (def pronunciations-trie
   (letfn [(word->nested-map [phones word]
             "To answer questions about the sequences of phonemes, we construct a trie and map over it with the appropriate substitution function."
@@ -104,6 +131,9 @@
   (pronun-valid? '("DH" "IY"))
   (pronun-valid? '("DH" "IY")))
 
+
+
+;;; Phonetic Substitutions
 (def equivalence-classes
   "It would be better to base these substitutions on some kind of linguistic data. I bet it exists."
   [#{"AA" "AO" "AW" "AH" "UH"}
@@ -139,6 +169,33 @@
   [phone]
   (apply vector (apply union (filter #(% phone) equivalence-classes))))
 
+
+
+;;; Dictionary Searching with Phonetic Substitutions
+(defn pronunciation->sentence
+  "Can this series of phonemes be parsed as a collection of English words? Find one answer containing no words from the set of words in the original sentence."
+  [pronunciation original-sentence]
+  (letfn [(word? [phones]
+            (and (not (original-sentence phones))
+                 (:word (pronun-valid? phones))))
+          (search-substitutions
+            [prev-phones next-phones]
+            (reduce (fn [acc x] (or acc (find-pronuns (conj prev-phones x) (rest next-phones))))
+                    nil
+                    (valid-replacements (first next-phones))))
+          (find-pronuns [prev-phones next-phones]
+            (let [answer (word? prev-phones)]
+              (cond
+                (empty? next-phones) (when answer (list answer))
+                answer (if-let [rest-of-sentence (find-pronuns [] next-phones)]
+                         (cons answer rest-of-sentence)
+                         (search-substitutions prev-phones next-phones))
+                :else (search-substitutions prev-phones next-phones))))]
+    (find-pronuns [] pronunciation)))
+
+
+
+;;; Interactivity
 (defn parse-sentence
   [sentence]
   (map word->pronunciations
@@ -179,29 +236,6 @@
    ("N" "AA" "T" "HH" "W" "AY" "L" "AY" "M" "IY" "T" "IH" "NG")
    ("N" "AA" "T" "HH" "W" "AY" "L" "AH" "M" "IY" "T" "IH" "NG")))
 
-
-(defn pronunciation->sentence
-  "Can this series of phonemes be parsed as a collection of English words? Find one answer containing no words from the set of words in the original sentence."
-  [pronunciation original-sentence]
-  (letfn [(word? [phones]
-            (and (not (original-sentence phones))
-                 (:word (pronun-valid? phones))))
-          (search-substitutions
-            [prev-phones next-phones]
-            (reduce (fn [acc x] (or acc (find-pronuns (conj prev-phones x) (rest next-phones))))
-                    nil
-                    (valid-replacements (first next-phones))))
-          (find-pronuns [prev-phones next-phones]
-            (let [answer (word? prev-phones)]
-              (cond
-                (empty? next-phones) (when answer (list answer))
-                answer (if-let [rest-of-sentence (find-pronuns [] next-phones)]
-                         (cons answer rest-of-sentence)
-                         (search-substitutions prev-phones next-phones))
-                :else (search-substitutions prev-phones next-phones))))]
-    (find-pronuns [] pronunciation)))
-
-
 (defn mondegreen
   [sentence]
   (let [parsed (parse-sentence sentence)
@@ -215,6 +249,9 @@
   (mondegreen "alpha kenny body")
   (mondegreen "I'll fuck any body")
   (mondegreen "I scream")
+  (mondegreen "Easel aid ease man")
+  (mondegreen "He's a lady's man")
   (mondegreen "The sky")
-  (mondegreen "Baby duckling"))
+  (mondegreen "Baby duckling")
+  )
 
